84ec9eb26308de1b2af8b4c9c7cb542b88f34a53
rene <rlatprnr@gmail.com>
2018-09-13 04:29:20 -0400

    install antd.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './redux/store';
+import 'antd/dist/antd.css';
 import './index.css';
 import Routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
