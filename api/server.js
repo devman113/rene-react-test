80e977f62de5ec9436ef059021a021d4a16bc0ce
rene <rlatprnr@gmail.com>
2018-09-13 04:23:03 -0400

    api server init

---
+++ api/server.js
@@ -0,0 +1,35 @@
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var Router = require('router');
+var api = Router();
+
+app.use('/api/', api);
+
+//Allow all requests from all domains & localhost
+app.all('/*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'POST, GET');
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var ingredients = {
+  data: {
+    textField: {
+
+    }
+  }
+};
+
+
+api.get('/configuration', function(req, res) {
+  console.log('GET From SERVER');
+  res.send(ingredients);
+});
+
+app.listen(3000);
